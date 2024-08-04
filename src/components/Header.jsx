217eae0e1616d2503d83249c06b1c38a106a136c
DarshanPro123 <darshanpro187@gmail.com>
2024-08-04 15:40:44 +0530

    css changeing and headewr styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <nav>
+        <h1>Cryprto coins</h1>
+        <main>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+          <NavLink to="/contact" activeClassName="active">
+            Contact
+          </NavLink>
+        </main>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
