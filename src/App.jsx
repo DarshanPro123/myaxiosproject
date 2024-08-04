950a564d66f7d59d0f4b3036d9593e55156b2161
DarshanPro123 <darshanpro187@gmail.com>
2024-08-04 15:21:28 +0530

    all files are routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
-  const [count, setCount] = useState(0);
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
+import header from "./styles/header.css";
+import home from "./styles/home.css";
+import about from "./styles/about.css";
+import contact from "./styles/contact.css";
+
+function App() {
   return (
     <>
-      <h1>App file</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
