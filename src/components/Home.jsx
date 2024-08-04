f98731424456d664f225f50b1471b07519342470
DarshanPro123 <darshanpro187@gmail.com>
2024-08-04 17:01:19 +0530

    some design changes and add npm axios api calling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const arra = [1, 2, 3, 4, 5, 6, 7];
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        {arra.map((index, value) => {
+          return <li key={index}>{value}</li>;
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
