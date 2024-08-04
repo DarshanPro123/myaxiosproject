24c722362c84b4abe80dfb94b99221e4cfde5b26
DarshanPro123 <darshanpro187@gmail.com>
2024-08-04 16:33:04 +0530

    conttact form created

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const messaPassed = (e) => {
+    e.preventDefault();
+    alert(`Name: ${name} \nEmail: ${email}`);
+    setEmail("");
+    setName("");
+  };
+
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="Example123@gg.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button onClick={messaPassed}>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
